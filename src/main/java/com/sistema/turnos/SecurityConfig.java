09e4a8de14be4d1d5acb5fbf1ff206b2c76f10d3
Facus006 <facu.calabrese06@gmail.com>
2026-06-12 00:37:43 -0300

    modificamos la seguridad para poder hacer el deploy

--- src/main/java/com/sistema/turnos/SecurityConfig.java
+++ src/main/java/com/sistema/turnos/SecurityConfig.java
@@ -59,7 +59,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://turno-flow-front.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
